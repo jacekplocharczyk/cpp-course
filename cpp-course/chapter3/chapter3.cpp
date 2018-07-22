8d1182a4b87f690dd8e34b534682d1e0b6f803d8
Jacek <jacek.plocharczyk@protonmail.com>
2018-07-22 21:36:58 +0200

    try this part

---
+++ cpp-course/chapter3/chapter3.cpp
@@ -0,0 +1,16 @@
+#include "drill.h"
+#include "exercises.h"
+#include "try_this.h"
+
+int main() {
+	//try_this1();
+	//try_this2();
+	//try_this3();
+	//try_this4();
+	try_this5();
+
+
+
+
+	return 0;
+}
